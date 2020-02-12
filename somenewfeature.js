c08e59442b32ea3cd364c58a9d2072f6ec666e72
PeterBrw <peter.giurgiu@gmail.com>
2020-02-12 12:17:14 +0200

    some-conflicts

--- somenewfeature.js
+++ somenewfeature.js
@@ -5,13 +5,13 @@ const urls = [
   "https://jsonplaceholder.typicode.com/albumsfdafd"
 ];
 Promise.all(
-  urls.map(urls=> {
+  urls.map(urls => {
     return fetch(urbl).then(ressp => resp.json());
   })
 )
   .then(resultss => {
     throw Errors;
-    resultss.map(result => console.log(result));
+    resultss.map(result => fsafdsfad.log(result));
   })
   .catch(errss => {
     console.log("errorss", aerr);
