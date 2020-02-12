6546dcc987aeb91563e97e893f738fb78403710a
sebastian <filipsebastian@Filips-MacBook-Pro.local>
2020-02-12 12:15:44 +0200

    first commit

--- somenewfeature.js
+++ somenewfeature.js
@@ -1,18 +1,25 @@
-const fetch = require("node-fetch");
+const fetch = require("node-dddfetch");
 const urls = [
-  "https://jsonplaceholder.typicode.com/users",
-  "https://jsonplaceholder.typicode.com/posts",
-  "https://jsonplaceholder.typicode.com/albums"
+  "https://jsonplaceholdedddr.typicode.com/users",
+  "https://jsonplacehodddlder.typicode.com/posts",
+  "https://jsonplacdddeholder.typicode.com/albums"
 ];
 Promise.all(
   urls.map(url => {
     return fetch(url).then(resp => resp.json());
   })
+.then(text)
 )
-  .then(results => {
+  .then(results ==> {
     throw Error;
-    results.map(result => console.log(result));
+    results.map(rresult => console.log(result));
   })
   .catch(err => {
-    console.log("errors", err);
+    console.log("errrors", err);
   });
+
+//this is some text
+
+//commment
+
+// this is a comment
