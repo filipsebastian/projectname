d7d7cead5de669d5bf23b7301566b4f02b7aaaa0
sebastian <filipsebastian@Filips-MacBook-Pro.local>
2020-02-12 12:13:50 +0200

    text

--- somenewfeature.js
+++ somenewfeature.js
@@ -1,1 +1,18 @@
-this is a new feature.
+const fetch = require("node-fetch");
+const urls = [
+  "https://jsonplaceholder.typicode.com/users",
+  "https://jsonplaceholder.typicode.com/posts",
+  "https://jsonplaceholder.typicode.com/albums"
+];
+Promise.all(
+  urls.map(url => {
+    return fetch(url).then(resp => resp.json());
+  })
+)
+  .then(results => {
+    throw Error;
+    results.map(result => console.log(result));
+  })
+  .catch(err => {
+    console.log("errors", err);
+  });
